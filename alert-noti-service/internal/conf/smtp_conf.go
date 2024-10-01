3e52d451d2412dfeb1f2840190a827b3209e4049
Tuan Trinh <tuan882612@gmail.com>
2024-10-01 11:44:03 -0500

    Update smtp_conf.go to use integer type for Port field

--- alert-noti-service/internal/conf/smtp_conf.go
+++ alert-noti-service/internal/conf/smtp_conf.go
@@ -2,7 +2,7 @@ package conf
 
 type smtp struct {
 	Host     string `env:"HOST, required"`
-	Port     string `env:"PORT, required"`
+	Port     int    `env:"PORT, required"`
 	Email    string `env:"EMAIL, required"`
 	Password string `env:"PASSWORD, required"`
 }

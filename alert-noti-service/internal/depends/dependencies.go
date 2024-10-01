3b58a681e52875e8849b18083fdfbe94f2fd9fc1
Tuan Trinh <tuan882612@gmail.com>
2024-10-01 11:43:53 -0500

    Add dependencies for alert notification service

---
+++ alert-noti-service/internal/depends/dependencies.go
@@ -0,0 +1,25 @@
+package depends
+
+import (
+	"github.com/prism-o11y/prism-server/alert-noti-service/internal/conf"
+	"github.com/prism-o11y/prism-server/alert-noti-service/internal/email/smtp"
+)
+
+type Dependencies struct {
+	Config       *conf.Config
+	SMTPProvider *smtp.Provider
+}
+
+func NewDependencies() (*Dependencies, error) {
+	conf, err := conf.New()
+	if err != nil {
+		return nil, err
+	}
+
+	smtpProvider := smtp.NewProvider(conf.Smtp.Host, conf.Smtp.Email, conf.Smtp.Password, conf.Smtp.Port)
+
+	return &Dependencies{
+		Config:       conf,
+		SMTPProvider: smtpProvider,
+	}, nil
+}

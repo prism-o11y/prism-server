138fb0bc62776d522c46262cd653af5e210325ee
DhrutikS257 <solankidhrutik0602@gmail.com>
2024-10-24 00:32:51 -0500

    Init implementation of consumer

--- user-service/src/config/base_config.py
+++ user-service/src/config/base_config.py
@@ -4,7 +4,6 @@ from pydantic_settings import BaseSettings
 from src.config.auth_config import Auth0Config
 from src.config.database_config import DatabaseConfig
 from src.config.server_config import ServerConfig
-from src.config.producer_config import ProducerConfig
 from src.config.auth_config import Auth0Config
 
 
@@ -12,12 +11,6 @@ class BaseConfig(BaseSettings):
     SERVER: ServerConfig = ServerConfig()
     AUTH0: Auth0Config = Auth0Config()
     DATABASE: DatabaseConfig = DatabaseConfig()
-
-    USER_PRODUCER: ProducerConfig = ProducerConfig(
-        bootstrap_servers="kafka:9092",
-        security_protocol="SASL_PLAINTEXT",
-        client_id="user-producer"
-    )
 
 @lru_cache()
 def get_base_config() -> BaseConfig:
--- user-service/src/svc/user/service.py
+++ user-service/src/svc/user/service.py
@@ -1,18 +1,13 @@
 from src.svc.user.repository import get_user_repository, UserRepository
 from fastapi import Depends
-from src.kafka.events import USER_EVENTS
-from src.kafka.producer import Producer, get_producer
-from src.kafka.message_model import KafkaMessage
 from src.svc.user.user_model import User
 import datetime as dt, uuid
 
 class UserService:
 
-    def __init__(self, userRepo: UserRepository, userProducer: Producer):
+    def __init__(self, userRepo: UserRepository):
         
         self.userRepo = userRepo
-
-        self.userProducer = userProducer
 
     async def register_user(self, email: str):
 
@@ -26,21 +21,12 @@ class UserService:
             last_login = None
         )
 
-        kafka_message = KafkaMessage(
-            event=USER_EVENTS.CREATED,
-            payload=user.model_dump(),
-            timestamp=dt.datetime.now()
-        )
-
-        self.userProducer.produce("user-topic", kafka_message)
-
         # return await self.userRepo.create_user(user)
     
     async def get_login_dates(self, user_id: str, access_token: str):
 
         pass
 
-async def get_user_service(userRepo: UserRepository = Depends(get_user_repository), 
-                           userProd: Producer = Depends(get_producer)) -> UserService:
+async def get_user_service(userRepo: UserRepository = Depends(get_user_repository)) -> UserService:
     
-    return UserService(userRepo,userProd)
+    return UserService(userRepo)

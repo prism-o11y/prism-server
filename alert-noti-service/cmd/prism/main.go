34f14ab142639d09fcb07cc82b53f2c932adf84a
Tuan Trinh <tuan882612@gmail.com>
2024-10-29 14:05:36 -0500

    Completed server struct

--- alert-noti-service/cmd/prism/main.go
+++ alert-noti-service/cmd/prism/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
-	"github.com/prism-o11y/prism-server/alert-noti-service/internal/alert"
 	"github.com/prism-o11y/prism-server/alert-noti-service/internal/depends"
+	"github.com/prism-o11y/prism-server/alert-noti-service/internal/server"
 )
 
 func main() {
+	level := zerolog.InfoLevel
+	_, ok := os.LookupEnv("ENVIRONMENT")
+	if ok {
+		level = zerolog.DebugLevel
+	}
+	zerolog.SetGlobalLevel(level)
+
 	logConfig := zerolog.ConsoleWriter{
 		Out:        os.Stderr,
 		TimeFormat: time.ANSIC,
@@ -20,18 +30,26 @@ func main() {
 
 	deps, err := depends.New()
 	if err != nil {
-		log.Fatal().Err(err).Msg("failed to load config")
+		log.Fatal().Err(err).Msg("Failed to load dependencies")
 	}
 
-	sampleData := &alert.Data{
-		Recipient: "tuan882612@gmail.com",
-		Severity:  alert.INFO,
-		Message:   "This is a sample message",
-		DateTime:  time.Now(),
+	srv, err := server.New(deps)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create server")
 	}
 
-	provider := deps.SMTPProvider
-	if err := provider.SendMail(sampleData); err != nil {
-		log.Error().Err(err).Msg("failed to send email")
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go srv.Start(ctx)
+
+	log.Info().Msg("Service is running")
+
+	<-ctx.Done()
+	log.Info().Msg("Shutting down gracefully...")
+
+	if err := srv.Stop(); err != nil {
+		log.Fatal().Err(err).Msg("Failed to shut down server")
 	}
+	log.Info().Msg("Server stopped")
 }

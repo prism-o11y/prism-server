c0dc0bcbf1ca9a71c4fe40457c02b6d20641f783
Tuan Trinh <tuan882612@gmail.com>
2024-11-06 09:37:14 -0600

    Add NodeID and NodeCount to server configuration

--- alert-noti-service/internal/conf/server_conf.go
+++ alert-noti-service/internal/conf/server_conf.go
@@ -3,6 +3,8 @@ package conf
 type Server struct {
 	Name           string `env:"NAME, required"`
 	Address        string `env:"EMAIL_ADDR, required"`
+	NodeID         string `env:"NODE, required"`
+	NodeCount      int    `env:"NODE_COUNT, required"`
 	AllowOrigins   Tokens `env:"ALLOWED_ORIGINS, required"`
 	AllowedHeaders Tokens `env:"ALLOWED_HEADERS, required"`
 }

24ca57ef4c563f83ee865f97e7ad066f177ec24d
Tuan Trinh <tuan882612@gmail.com>
2024-11-14 10:50:39 -0600

    Implement distributed lock with acquire, release, renew, and get node functionality

--- alert-noti-service/internal/notify/sse/lock/distributed_lock.go
+++ alert-noti-service/internal/notify/sse/lock/distributed_lock.go
@@ -1,13 +1,94 @@
 package lock
 
-import "github.com/go-redis/redis"
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/rs/zerolog/log"
+)
+
+const DefaultLockTTL = 2 * time.Minute
+
+var (
+	ErrLockAlreadyHeld = fmt.Errorf("lock already held")
+	ErrNoLockFound     = fmt.Errorf("no lock found")
+
+	ErrFailedToAcquireLock = fmt.Errorf("failed to acquire lock")
+	ErrFailedToReleaseLock = fmt.Errorf("failed to release lock")
+	ErrFailedToRenewLock   = fmt.Errorf("failed to renew lock")
+	ErrFailedToGetNode     = fmt.Errorf("failed to get node")
+)
 
 type DistributedLock struct {
 	client *redis.Client
+	ttl    time.Duration
 }
 
-func NewDistributedLock(client *redis.Client) *DistributedLock {
+func NewDistributedLock(client *redis.Client, ttl time.Duration) *DistributedLock {
 	return &DistributedLock{
 		client: client,
+		ttl:    ttl,
 	}
+}
+
+func (d *DistributedLock) Acquire(clientID, nodeID string) error {
+	key := d.getClientKey(clientID)
+
+	ok, err := d.client.SetNX(key, nodeID, d.ttl).Result()
+	if err != nil {
+		log.Error().Err(err).Str("client_id", clientID).Msg("Failed to acquire lock")
+		return ErrFailedToAcquireLock
+	}
+	if !ok {
+		log.Warn().Str("client_id", clientID).Msg("Lock already held by another node")
+		return ErrLockAlreadyHeld
+	}
+
+	log.Info().Str("client_id", clientID).Str("node_id", nodeID).Msg("Lock acquired")
+	return nil
+}
+
+func (d *DistributedLock) Release(clientID string) error {
+	key := d.getClientKey(clientID)
+
+	_, err := d.client.Del(key).Result()
+	if err != nil {
+		log.Error().Err(err).Str("client_id", clientID).Msg("Failed to release lock")
+		return ErrFailedToReleaseLock
+	}
+
+	log.Info().Str("client_id", clientID).Msg("Lock released")
+	return nil
+}
+
+func (d *DistributedLock) Renew(clientID string) error {
+	key := d.getClientKey(clientID)
+
+	_, err := d.client.Expire(key, d.ttl).Result()
+	if err != nil {
+		log.Error().Err(err).Str("client_id", clientID).Msg("Failed to renew lock")
+		return ErrFailedToRenewLock
+	}
+
+	log.Info().Str("client_id", clientID).Msg("Lock renewed successfully")
+	return nil
+}
+
+func (d *DistributedLock) GetNodeForClient(clientID string) (string, error) {
+	key := d.getClientKey(clientID)
+
+	nodeID, err := d.client.Get(key).Result()
+	if err == redis.Nil {
+		return "", ErrNoLockFound
+	}
+	if err != nil {
+		return "", ErrFailedToGetNode
+	}
+
+	return nodeID, nil
+}
+
+func (d *DistributedLock) getClientKey(clientID string) string {
+	return fmt.Sprintf("client:%s", clientID)
 }

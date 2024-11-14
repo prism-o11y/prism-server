c8fbdf0c8c5997659b8760e671e1f989ecc819a4
Tuan Trinh <tuan882612@gmail.com>
2024-11-14 10:51:53 -0600

    Enhance SSE client management with event sending, heartbeat, and disconnection handling

--- alert-noti-service/internal/notify/handler.go
+++ alert-noti-service/internal/notify/handler.go
@@ -2,10 +2,12 @@ package notify
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
-	"github.com/prism-o11y/prism-server/shared/data"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/prism-o11y/prism-server/shared/data/kafka"
 	"github.com/rs/zerolog/log"
 
@@ -15,33 +17,76 @@ import (
 )
 
 type Handler struct {
-	eventSender *sse.EventSender
-	emailSender *smtp.EmailSender
-	consManager *kafka.ConsumerManager
+	eventSender     *sse.EventSender
+	emailSender     *smtp.EmailSender
+	producerManager *kafka.ProducerManager
+	consManager     *kafka.ConsumerManager
+	nodeID          int
 }
 
-func NewHandler(eventSender *sse.EventSender, emailSender *smtp.EmailSender, consManager *kafka.ConsumerManager) *Handler {
+func NewHandler(eventSender *sse.EventSender, emailSender *smtp.EmailSender, producerManager *kafka.ProducerManager, consManager *kafka.ConsumerManager, nodeID int) *Handler {
 	return &Handler{
-		eventSender: eventSender,
-		emailSender: emailSender,
-		consManager: consManager,
+		eventSender:     eventSender,
+		emailSender:     emailSender,
+		producerManager: producerManager,
+		consManager:     consManager,
+		nodeID:          nodeID,
 	}
 }
 
 func (h *Handler) SSEHandler(w http.ResponseWriter, r *http.Request) {
+	clientID := r.URL.Query().Get("client_id")
+	nodeIDStr := fmt.Sprintf("%d", h.nodeID)
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		log.Error().Msg("Streaming unsupported")
+		http.Error(w, "Streaming unsupported", http.StatusInternalServerError)
+		return
+	}
+
+	flusher.Flush()
+
+	client, err := sse.NewClient(clientID, w)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to create client")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = h.eventSender.CliManager.AddClient(clientID, nodeIDStr, client)
+	if err != nil {
+		if err.Error() == fmt.Sprintf("client %s is already connected to another node", clientID) {
+			http.Error(w, "Client is connected to another node", http.StatusConflict)
+			return
+		}
+		log.Error().Err(err).Msg("Failed to add client")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	log.Info().Str("client_id", clientID).Msg("SSE connection established")
+
+	<-client.DisconnectChan
+
+	log.Info().Str("client_id", clientID).Msg("SSE connection closed")
 }
 
-func (h *Handler) StartConsumers(ctx context.Context, brokers []string, topics []string, groupIDs []string, partition int, timeout time.Duration) {
+func (h *Handler) StartConsumers(ctx context.Context, brokers []string, topics []string, groupIDs []string, timeout time.Duration) {
 	for i, topic := range topics {
 		err := h.consManager.AddConsumer(
 			brokers,
 			topic,
 			groupIDs[i],
-			partition,
+			h.nodeID,
 			timeout,
-			func(msg []byte) error {
-				return h.processMessage(msg)
-			},
+			h.processMessage,
 		)
 		if err != nil {
 			log.Error().Err(err).Str("topic", topic).Msg("Failed to add consumer")
@@ -54,34 +99,85 @@ func (h *Handler) StartConsumers(ctx context.Context, brokers []string, topics [
 }
 
 func (h *Handler) processMessage(msg []byte) error {
-	event, err := data.NewEventData(msg)
+	notification, err := models.ParseNotification(msg)
 	if err != nil {
 		return err
 	}
 
-	request, err := models.ParseNotifyRequest(event.Data)
-	if err != nil {
-		return err
+	switch n := notification.(type) {
+	case *models.SMTPNotification:
+		return h.handleSMTPNotification(n)
+	case *models.SSENotification:
+		return h.handleSSENotification(n)
+	default:
+		return fmt.Errorf("unknown notification type")
 	}
+}
 
+func (h *Handler) handleSMTPNotification(n *models.SMTPNotification) error {
 	retries, backoff := 3, time.Second*2
 	for i := 0; i < retries; i++ {
-		switch request.Action {
-		case models.SMTP:
-			if err := h.emailSender.SendEmail(request); err != nil {
-				log.Warn().Err(err).Msgf("Retrying to send email (%d/%d)", i+1, retries)
-				time.Sleep(backoff)
-				backoff *= 2
-				continue
-			}
-
-			log.Info().Str("recipient", request.Recipient).Msg("Email sent successfully")
-			return nil
-		case models.SSE:
-			log.Info().Str("recipient", request.Recipient).Msg("Sending SSE event")
+		if err := h.emailSender.SendEmail(n); err != nil {
+			log.Warn().Err(err).Msgf("Retrying to send email (%d/%d)", i+1, retries)
+			time.Sleep(backoff)
+			backoff *= 2
+			continue
 		}
+
+		log.Info().Str("recipient", n.Recipient).Msg("Email sent successfully")
+		return nil
 	}
 
-	log.Error().Err(err).Str("recipient", request.Recipient).Msg("Failed to send email after retries")
-	return err
+	log.Error().Str("recipient", n.Recipient).Msg("Failed to send email after retries")
+	return fmt.Errorf("failed to send email to %s", n.Recipient)
+}
+
+func (h *Handler) handleSSENotification(n *models.SSENotification) error {
+	clientID := n.ClientID
+	nodeIDStr := fmt.Sprintf("%d", h.nodeID)
+
+	lockNodeID, exist := h.eventSender.CliManager.HasClient(clientID, nodeIDStr)
+	if exist {
+		err := h.eventSender.SendEventToClient(clientID, n)
+		if err != nil {
+			log.Error().Err(err).Str("client_id", clientID).Msg("Failed to send event to local client")
+			return err
+		}
+		log.Info().Str("client_id", clientID).Msg("Event sent to local client successfully")
+		return nil
+	} else {
+		nodeID, err := h.eventSender.CliManager.GetNodeForClient(clientID)
+		if err != nil {
+			log.Error().Err(err).Str("client_id", clientID).Msg("Failed to get node for client")
+			return err
+		}
+
+		err = h.forwardMessageToNode(nodeID, lockNodeID, n)
+		if err != nil {
+			log.Error().Err(err).Str("node_id", nodeID).Msg("Failed to forward message to node")
+			return err
+		}
+		log.Info().Str("node_id", nodeID).Msg("Message forwarded to node successfully")
+		return nil
+	}
+}
+
+func (h *Handler) forwardMessageToNode(nodeID string, lockNodeID string, notification *models.SSENotification) error {
+	msg, err := jsoniter.Marshal(notification)
+	if err != nil {
+		return err
+	}
+
+	lockIDInt, err := strconv.Atoi(lockNodeID)
+	if err != nil {
+		log.Err(err).Msg("Failed to convert lock node ID to int")
+		return err
+	}
+
+	err = h.producerManager.ProduceToPartition(kafka.TransferTopic, lockIDInt, []byte(nodeID), msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- alert-noti-service/internal/notify/sse/sender.go
+++ alert-noti-service/internal/notify/sse/sender.go
@@ -1,15 +1,32 @@
 package sse
 
 import (
+	"fmt"
+
+	"github.com/google/uuid"
+
+	"github.com/prism-o11y/prism-server/alert-noti-service/internal/notify/models"
 	"github.com/prism-o11y/prism-server/alert-noti-service/internal/notify/sse/lock"
 )
 
 type EventSender struct {
-	cliManager *clientManager
+	CliManager *clientManager
 }
 
 func NewEventSender(cacheManager *lock.DistributedLock) *EventSender {
 	return &EventSender{
-		cliManager: newClientManager(cacheManager),
+		CliManager: newClientManager(cacheManager),
 	}
+}
+
+func (es *EventSender) SendEventToClient(clientID string, notification *models.SSENotification) error {
+	client, exists := es.CliManager.GetClient(clientID)
+	if !exists {
+		return fmt.Errorf("client %s not connected to this node", clientID)
+	}
+
+	data := notification.Message
+	eventID := uuid.New().String()
+	eventType := "alert"
+	return client.SendEvent(eventID, eventType, data)
 }

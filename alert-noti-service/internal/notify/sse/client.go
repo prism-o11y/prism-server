546d60aa8f38d05da62de1759b030cd419bc25d2
Tuan Trinh <tuan882612@gmail.com>
2024-11-14 01:07:49 -0600

    Implement SSE handling and add distributed lock for client management

--- alert-noti-service/internal/notify/sse/client.go
+++ alert-noti-service/internal/notify/sse/client.go
@@ -1,1 +1,12 @@
 package sse
+
+import "github.com/google/uuid"
+
+type Client struct{
+	ClientID uuid.UUID `json:"client_id"`
+	
+}
+
+func NewClient() *Client {
+	return &Client{}
+}
--- alert-noti-service/internal/notify/sse/client_manager.go
+++ alert-noti-service/internal/notify/sse/client_manager.go
@@ -1,7 +1,20 @@
 package sse
 
-type clientManager struct{}
+import (
+	"sync"
 
-func newClientManager() *clientManager {
-	return &clientManager{}
+	"github.com/prism-o11y/prism-server/alert-noti-service/internal/notify/sse/lock"
+)
+
+type clientManager struct {
+	clients  map[string]*Client
+	distLock *lock.DistributedLock
+	mu       sync.RWMutex
+}
+
+func newClientManager(distLock *lock.DistributedLock) *clientManager {
+	return &clientManager{
+		clients:  make(map[string]*Client),
+		distLock: distLock,
+	}
 }

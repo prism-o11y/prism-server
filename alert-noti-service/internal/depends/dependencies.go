45cd1959676269d85293fb9f13ec9031858d94ba
Tuan Trinh <tuan882612@gmail.com>
2024-11-14 01:07:11 -0600

    Add Redis configuration and client to alert notification service

--- alert-noti-service/internal/depends/dependencies.go
+++ alert-noti-service/internal/depends/dependencies.go
@@ -3,18 +3,21 @@ package depends
 import (
 	"context"
 
+	"github.com/go-redis/redis"
 	"github.com/prism-o11y/prism-server/shared/data/kafka"
+	"github.com/prism-o11y/prism-server/shared/data/rconn"
+	"github.com/rs/zerolog/log"
 
 	"github.com/prism-o11y/prism-server/alert-noti-service/internal/conf"
 	"github.com/prism-o11y/prism-server/alert-noti-service/internal/notify"
 	"github.com/prism-o11y/prism-server/alert-noti-service/internal/notify/smtp"
 	"github.com/prism-o11y/prism-server/alert-noti-service/internal/notify/sse"
+	"github.com/prism-o11y/prism-server/alert-noti-service/internal/notify/sse/lock"
 )
 
 type Dependencies struct {
+	redisClient   *redis.Client
 	Config        *conf.Config
-	EmailSender   *smtp.EmailSender
-	EventSender   *sse.EventSender
 	ConsManager   *kafka.ConsumerManager
 	NotifyHandler *notify.Handler
 }
@@ -30,14 +33,19 @@ func New() (*Dependencies, error) {
 		return nil, err
 	}
 
-	consManager := kafka.NewConsumerManager()
-	eventSender := sse.NewEventSender()
+	client, err := rconn.NewConnection(cfg.Databases.RedisURL, cfg.Databases.RedisPassword)
+	if err != nil {
+		return nil, err
+	}
 
+	consManager := kafka.NewConsumerManager()
+	cacheManager := lock.NewDistributedLock(client)
+	eventSender := sse.NewEventSender(cacheManager)
 	notifyHandler := notify.NewHandler(eventSender, emailSender, consManager)
 
 	return &Dependencies{
 		Config:        cfg,
-		EmailSender:   emailSender,
+		redisClient:   client,
 		ConsManager:   consManager,
 		NotifyHandler: notifyHandler,
 	}, nil
@@ -45,5 +53,11 @@ func New() (*Dependencies, error) {
 
 func (d *Dependencies) Close(ctx context.Context) error {
 	d.ConsManager.CloseAllConsumers()
+
+	if err := d.redisClient.Close(); err != nil {
+		log.Error().Err(err).Msg("Failed to close redis client")
+		return err
+	}
+
 	return nil
 }

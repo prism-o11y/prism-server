989e9b68ee4b7ca9d5f50e2c9f795f048b34d8b8
Tuan Trinh <tuan882612@gmail.com>
2024-12-01 12:49:58 -0600

    Updated the SSE model

--- alert-noti-service/internal/notify/models/sse.go
+++ alert-noti-service/internal/notify/models/sse.go
@@ -6,7 +6,7 @@ type SSENotification struct {
     ClientID      string        `json:"client_id"`
     Severity      AlertSeverity `json:"severity"`
     Message       string        `json:"message"`
-    DateTime      time.Time     `json:"dateTime"`
+    DateTime      time.Time     `json:"datetime"`
     TargetNodeID  string        `json:"target_node_id,omitempty"`
     OriginNodeID  string        `json:"origin_node_id,omitempty"`
     IsForwarded   bool          `json:"is_forwarded,omitempty"`

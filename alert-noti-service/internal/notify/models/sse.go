37b0c3d77259d142a709005aef8bbd0bdd640320
Tuan Trinh <tuan882612@gmail.com>
2024-12-03 15:08:18 -0600

    Added in manual conn id insertion and removed all to single event broadcasting

--- alert-noti-service/internal/notify/models/sse.go
+++ alert-noti-service/internal/notify/models/sse.go
@@ -3,11 +3,12 @@ package models
 import "time"
 
 type SSENotification struct {
-    ClientID      string        `json:"client_id"`
-    Severity      AlertSeverity `json:"severity"`
-    Message       string        `json:"message"`
-    DateTime      time.Time     `json:"datetime"`
-    TargetNodeID  string        `json:"target_node_id,omitempty"`
-    OriginNodeID  string        `json:"origin_node_id,omitempty"`
-    IsForwarded   bool          `json:"is_forwarded,omitempty"`
+	ClientID     string        `json:"client_id"`
+	ConnectionID string        `json:"connection_id"`
+	Severity     AlertSeverity `json:"severity"`
+	Message      string        `json:"message"`
+	DateTime     time.Time     `json:"datetime"`
+	TargetNodeID string        `json:"target_node_id,omitempty"`
+	OriginNodeID string        `json:"origin_node_id,omitempty"`
+	IsForwarded  bool          `json:"is_forwarded,omitempty"`
 }

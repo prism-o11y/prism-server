d7ad1b49b44f3a27a68747a684de672ff30f0bc3
Tuan Trinh <tuan882612@gmail.com>
2024-12-08 16:33:12 -0600

    Add log write and read topics and group IDs to Kafka constants

--- shared/data/kafka/constants.go
+++ shared/data/kafka/constants.go
@@ -3,9 +3,13 @@ package kafka
 const (
 	TransferTopic = "transfer-topic"
 	NotifyTopic   = "notify-topic"
+	LogWriteTopic = "log-write-topic"
+	LogReadTopic  = "log-read-topic"
 )
 
 const (
-	NotifyGroupID = "notify-group"
+	NotifyGroupID   = "notify-group"
 	TransferGroupID = "transfer-group"
+	LogWriteGroupID = "log-write-group"
+	LogReadGroupID  = "log-read-group"
 )
